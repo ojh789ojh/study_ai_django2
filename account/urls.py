0284b4897904f43a496b003c965a5d0aa0aa0fd1
DESKTOP-KKKOTUV\ojh789 <ojh789ojh@naver.com>
2021-07-12 12:48:24 +0900

    no message

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from account.views import hello_world
 
+app_name = 'account'
+
 urlpatterns = [
     path('hello_world/', hello_world, name="hello_world"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,12 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from account.models import HelloWorld
 
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+        temp = request.POST.get("input_text")
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('account:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'account/hello_world.html', context={'text': 'GET METHOD!',
+                                                                    'hello_world_list': hello_world_list})
 
