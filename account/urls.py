e09a26336a62d4608acbe457cc8d7788460bbd8b
DESKTOP-KKKOTUV\ojh789 <ojh789ojh@naver.com>
2021-07-15 12:51:09 +0900

    no message

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from account.views import hello_world, AccountCreate
+from account.views import hello_world, AccountCreate, AccountDetail
 
 app_name = 'account'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('create/', AccountCreate.as_view(), name='create'),
     path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('detail/<int:pk>', AccountDetail.as_view(), name='detail'),
 ]
--- account/views.py
+++ account/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from account.models import HelloWorld
 
@@ -30,3 +30,9 @@ class AccountCreate(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('account:hello_world')
     template_name = 'account/create.html'
+
+
+class AccountDetail(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'account/detail.html'
