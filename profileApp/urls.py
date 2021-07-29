88bf88f35f38a0a7039dad5be6cf1104d58e39f0
DESKTOP-KKKOTUV\ojh789 <ojh789ojh@naver.com>
2021-07-29 11:24:20 +0900

    no message

--- profileApp/urls.py
+++ profileApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from profileApp.views import ProfileCreationView
+from profileApp.views import ProfileCreationView, ProfileUpdateView
 
 app_name = 'profile'
 
 urlpatterns = [
     path('create/', ProfileCreationView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
--- profileApp/views.py
+++ profileApp/views.py
@@ -1,15 +1,34 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileApp.decorators import profile_ownership_required
 from profileApp.forms import ProfileCreationForm
 from profileApp.models import Profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreationView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
     success_url = reverse_lazy('account:hello_world')
     template_name = 'profile/create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('account:hello_world')
+    template_name = 'profile/update.html'
