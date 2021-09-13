406b3aab0dba1a86f5d181e43bfec25f8acb54f3
DESKTOP-KKKOTUV\ojh789 <ojh789ojh@naver.com>
2021-09-13 12:48:50 +0900

    no message

--- gis_1/settings/base.py
+++ gis_1/settings/base.py
@@ -16,19 +16,11 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4iwnk1)f9d2k+fo=!4pa*+f57)!i*jg6s*iqphca1i=0c_td*b'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -90,12 +82,7 @@ WSGI_APPLICATION = 'gis_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
